8186d0abb22a5ebd44b2362f7ec5a1be07849a15
HanNhat02 <hannhat2468@gmail.com>
2024-10-08 10:14:20 +0700

    call api voi tmbd

--- src/components/FeatureMovies/Movie.jsx
+++ src/components/FeatureMovies/Movie.jsx
@@ -1,32 +1,26 @@
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const Movie = () => {
+const Movie = ({ data }) => {
+  const { backdrop_path, title, release_date, overview } = data || {}; // Kiểm tra nếu 'data' tồn tại
   return (
     <div>
       <img
         className="aspect-video brightness-50"
-        src="https://plus.unsplash.com/premium_photo-1724824053224-40d726301acf?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
         alt=""
       />
       <div className="absolute bottom-[10%] left-8 w-1/2 sm:w-1/3">
-        <p className="mb-2 font-bold sm:text-[2vw]">Inside Out 2</p>
+        <p className="mb-2 font-bold sm:text-[2vw]">{title}</p>
         <div>
           <p className="mb-1 inline-block border border-gray-400 p-1 text-gray-400">
             PG13
           </p>
-          <p className="text-[1.2vw]">2024-06-11</p>
+          <p className="text-[1.2vw]">{release_date}</p>
         </div>
         <div>
           <div className="mt-4 hidden sm:block">
             <p className="mb-2 font-bold">Overview</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium possimus eius libero repellendus alias dolorum,
-              architecto velit consequuntur totam natus animi rem quos. Eveniet
-              vitae, cupiditate id voluptatibus cum in facilis, omnis,
-              repudiandae aspernatur deleniti alias architecto laborum. Cumque,
-              blanditiis.
-            </p>
+            <p>{overview}</p>
           </div>
           <div className="mt-4">
             <button className="text-10 lg:text-2 mr-4 rounded bg-white px-4 py-2 text-black">
